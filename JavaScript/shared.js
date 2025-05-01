10220ca2df468dadb736086ce8daa1ee0a0a083f
Mobolaji Adewale <96749708+Bolajiadewal3@users.noreply.github.com>
2025-05-01 15:08:41 -0700

    Updates

--- JavaScript/shared.js
+++ JavaScript/shared.js
@@ -4,11 +4,11 @@ $( document ).ready(function() {
 
 
     $("body").mousedown(function() {
-        $(this).css("cursor", "url(./Cursors/arrowhead-hover.svg), auto")
+        $(this).css("cursor", "url(../Cursors/arrowhead-hover.svg), auto")
     });
 
     $("body").mouseup(function() {
-        $(this).css("cursor", "url(./Cursors/arrowhead-mousedown.svg), auto")
+        $(this).css("cursor", "url(../Cursors/arrowhead-mousedown.svg), auto")
     });
 
 
