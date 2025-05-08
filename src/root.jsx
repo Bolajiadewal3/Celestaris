3374060a412baa7189daa581b93507c0f3cd3359
Mobolaji Adewale <96749708+Bolajiadewal3@users.noreply.github.com>
2025-05-08 16:53:05 -0700

    changes II

--- src/root.jsx
+++ src/root.jsx
@@ -2,7 +2,7 @@ import { createRoot } from "react-dom/client";
 import React from "react";
 import App from "./city.jsx";
 import Section0 from "./Poetry/section0"; // HTML-based poetry site
-import existentialPoetry from "./Poetry/existentialPoetry"; // HTML-based poetry site
+import ExistentialPoetry from "./Poetry/existentialPoetry"; // HTML-based poetry site
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
@@ -13,7 +13,7 @@ createRoot(document.getElementById("root")).render(
       <Route path="/Celestaris/Poetry/section0" element={<Section0 />} />
       <Route
         path="/Celestaris/Poetry/existentialPoetry"
-        element={<existentialPoetry />}
+        element={<ExistentialPoetry />}
       />
     </Routes>
   </BrowserRouter>
