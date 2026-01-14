b9b173fe4a22e3a80a22d9f2c23aa5a62daad28b
Mobolaji Adewale <96749708+Bolajiadewal3@users.noreply.github.com>
2026-01-14 20:10:45 +0000

    wordpress2

--- portfolio.jsx
+++ portfolio.jsx
@@ -1,3 +1,7 @@
+import React from "react";
+import { Canvas } from "@react-three/fiber";
+import { Html } from "@react-three/drei";
+
 /**
  * Main application component rendering a Three.js city scene,
  * interactive UI overlays, banners, and ambient experience.
@@ -8,13 +12,22 @@
 export default function Portfolio() {
   return (
     <div style={{ width: "100vw", height: "100vh", position: "relative" }}>
-      <mesh>
-        <planeGeometry args={[1.2, 0.9]} />
-        <meshStandardMaterial color="black" />
-        <Html transform occlude>
-          <iframe src="https://aremuart.wordpress.com/" />
-        </Html>
-      </mesh>
+      <Canvas>
+        <ambientLight intensity={0.5} />
+        <pointLight position={[10, 10, 10]} />
+
+        <mesh position={[0, 0, 0]}>
+          <planeGeometry args={[1.2, 0.9]} />
+          <meshStandardMaterial color="black" />
+
+          <Html transform occlude>
+            <iframe
+              src="https://aremuart.wordpress.com/"
+              style={{ width: "1024px", height: "768px", border: "none" }}
+            />
+          </Html>
+        </mesh>
+      </Canvas>
     </div>
   );
 }
