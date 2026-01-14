c30e162376e459c81251fee1c43ae33354fcb9da
Mobolaji Adewale <96749708+Bolajiadewal3@users.noreply.github.com>
2026-01-14 20:44:31 +0000

    wordpress2

--- portfolio.jsx
+++ portfolio.jsx
@@ -12,7 +12,7 @@ import { Html, OrbitControls } from "@react-three/drei";
 export default function Portfolio() {
   return (
     <div style={{ width: "100vw", height: "100vh", position: "relative" }}>
-      <Canvas camera={{ position: [0, 1, 3], fov: 50 }}>
+      <Canvas camera={{ position: [0, 1, 5], fov: 50 }}>
         <ambientLight intensity={0.5} />
         <pointLight position={[10, 10, 10]} />
 
@@ -23,13 +23,18 @@ export default function Portfolio() {
           <Html
             transform
             occlude
-            position={[0, 0, 0.01]} // slightly in front of plane
             center
-            style={{ width: "100%", height: "100%" }}
+            position={[0, 0, 0.01]}
+            style={{
+              width: "1024px",
+              height: "768px",
+              transform: "scale(0.0012)", // tweak this until it fits your plane
+              transformOrigin: "top left",
+            }}
           >
             <iframe
               src="https://aremuart.wordpress.com/"
-              style={{ width: "100%", height: "100%", border: "none" }}
+              style={{ width: "1024px", height: "768px", border: "none" }}
               sandbox="allow-scripts allow-same-origin allow-forms allow-popups"
               scrolling="yes"
             />
